0c8becaf498a53a618180e9ff738eeb3d60ae5c4
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2022-03-21 18:08:04 +0100

    feat: basic project done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from process import extract_facial_expression_from_image, train_or_load_facial_expression_recognition_model
+from process import extract_diabetic_retinopathy_stage_from_image, train_or_load_diabetic_retinopathy_stage_recognition_model
 import glob
 import sys
 import os
@@ -17,40 +17,39 @@ else:
 # -------------------------------------------------------------------
 
 # indeksiranje labela za brzu pretragu
-label_dict = dict()
-with open(TRAIN_DATASET_PATH+'annotations.csv', 'r') as file:
-    lines = file.readlines()
-    for index, line in enumerate(lines):
-        if index > 0:
-            cols = line.replace('\n', '').split(',')
-            label_dict[cols[0]] = cols[1]
-
 # priprema skupa podataka za metodu za treniranje
 train_image_paths = []
 train_image_labels = []
-for image_name in os.listdir(TRAIN_DATASET_PATH):
-    if '.jpg' in image_name:
-        train_image_paths.append(os.path.join(TRAIN_DATASET_PATH, image_name))
-        train_image_labels.append(label_dict[image_name])
+label_dict = dict()
 
-# istrenirati model za prepoznavanje ekspresije lica
-model = train_or_load_facial_expression_recognition_model(train_image_paths, train_image_labels)
+for root, dirs, files in os.walk(TRAIN_DATASET_PATH):
+    path = root.split(os.sep)
+    for file in files:
+        if len(path) == 4:
+            label_dict[file] = path[3]
+            train_image_paths.append(os.path.join(TRAIN_DATASET_PATH, path[3], file))
+            train_image_labels.append(path[3])
+
+# istrenirati model za prepoznavanje faze dijabetesne retinopatije
+model = train_or_load_diabetic_retinopathy_stage_recognition_model(train_image_paths, train_image_labels)
 
 # izvrsiti citanje teksta sa svih fotografija iz validacionog skupa podataka, koriscenjem istreniranog modela
 processed_image_names = []
-extracted_facial_expression = []
+extracted_retinopathy_stage = []
 
-for image_path in glob.glob(VALIDATION_DATASET_PATH + "*.jpg"):
-    image_directory, image_name = os.path.split(image_path)
-    processed_image_names.append(image_name)
-    extracted_facial_expression.append(extract_facial_expression_from_image(model, image_path))
-
+for root, dirs, files in os.walk(VALIDATION_DATASET_PATH):
+    path = root.split(os.sep)
+    for file in files:
+        if len(path) == 4:
+            processed_image_names.append(file)
+            image_path = os.path.join(VALIDATION_DATASET_PATH, path[3], file)
+            extracted_retinopathy_stage.append(extract_diabetic_retinopathy_stage_from_image(model, image_path))
 
 # -----------------------------------------------------------------
 # Kreiranje fajla sa rezultatima ekstrakcije za svaku sliku
 result_file_contents = ""
 for image_index, image_name in enumerate(processed_image_names):
-    result_file_contents += "%s,%s\n" % (image_name, extracted_facial_expression[image_index])
+    result_file_contents += "%s,%s\n" % (image_name, extracted_retinopathy_stage[image_index])
 # sacuvaj formirane rezultate u csv fajl
 with open('result.csv', 'w') as output_file:
     output_file.write(result_file_contents)
